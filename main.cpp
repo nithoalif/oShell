bfdab138033ea47efbf377b09c02b9a831f8e946
Nitho Alif Ibadurrahman <nithoalif@yahoo.com>
2015-03-19 09:50:18 +0700

    New codebase

--- main.cpp
+++ main.cpp
@@ -2,89 +2,33 @@
 /* Author	: Afrizal Fikri (13513004)			*/
 /* 			  Nitho Alif I. (13513072)			*/
 /* 			  Fiqie Ulya Sidiastahta (13513602)	*/
-
-#include "shell.h"
-
-#define READ 	0
-#define WRITE 	1
-
+#include <iostream>
+#include <vector>
+#include <sys/wait.h>
+#include "shellC.h"
 using namespace std;
 
-pid_t childPid;
-int fd_snd[2];  // file descriptor send
-int fd_rcv[2];  // file descriptor receieve
-
-char buf[1<<20];	// buffer
-
-int main(int argc, char **argv)
-{
-	printf("%s", Clear);
-
-	pipe(fd_snd);
-	pipe(fd_rcv);
-
-	while (true) 
-	{
-		printPrompt();
-
-		string cmdLine = readCmd();
-		queue<string> cmdAll = parseCmd(cmdLine);
-		
-		while (!cmdAll.empty())
-		{
-			vector<string> cmd;
-
-			while (!cmdAll.empty() && cmdAll.front() != "|") {
-				cmd.push_back(cmdAll.front());
-				cmdAll.pop();
-			}
-			if (!cmdAll.empty()) cmdAll.pop();
-
-			if (isBuiltInCommand(cmd))
-			{
-				//dup2(1, fd[0]);		/* redirect stdout to pipe-in */
-				
-				executeBuiltInCommand(cmd);
-			}
-			else
-			{
-				childPid = fork();
-				if (childPid == 0)		/* child */
-				{
-					write(fd_snd[WRITE], buf, strlen(buf) + 1);
-
-					close(fd_snd[WRITE]);
-					close(fd_rcv[READ]);
-
-					dup2(fd_rcv[WRITE], STDOUT_FILENO);		/* redirect stdout to pipe-in */
-					dup2(fd_snd[READ], STDIN_FILENO);		/* redirect pipe-out to stdin */
-
-					executeCommand(cmd);
-				} 
-				else 			/* parent */
-				{
-					int status;
-					while (wait(&status) != childPid);
-					//cout << "cok" << endl;
-
-					/* redirecting */
-					read(fd_rcv[READ], buf, sizeof(buf));
-					puts(buf);
-				}
-
-				/* else{
-					if (isBackgroundJob(cmd)){
-
-					} else{
-						waitpid(childPid);
-					}
-				}*/
+int main(int argc, char const *argv[]){
+	cout << Clear;
+	shellC oShell;
+	vector<string> cmd;
+	string cmdLine;
+	int status;
+	while(1){
+		cmd.clear();
+		cmdLine = oShell.readCmd();
+		cmd = oShell.parseCmd(cmdLine);
+		if(oShell.isBuiltInCmd(cmd)){
+			oShell.execBuiltInCmd(cmd);
+		} else{
+			pid_t pid = fork();
+			if(pid == 0){
+				oShell.execExternCmd(cmd);
+			} else{
+				wait(&status);				
 			}
 		}
-
-		puts(buf);
 	}
-	puts("");
+
 	return 0;
 }
-
